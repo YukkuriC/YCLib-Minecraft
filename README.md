094e8662ab2c563f643f90aa0d097e3e9a572a3c
YukkuriC <799433638@qq.com>
2026-09-06 13:58:59 +0800

    simplify installation

--- README.md
+++ README.md
@@ -1,50 +1,5 @@
 # Setup
-## Download me (into `$ROOT/yclib`)
+Download me (into `$ROOT/yclib`) and load me as dep
 ```groovy
-// add at head
-def jarFile = file("$projectDir/yclib/yclib-${yclib_version}.jar")
-if (!jarFile.exists()) {
-    def jarUrl = "https://github.com/YukkuriC/YCLib-Minecraft/releases/download/$yclib_version/yclib-${yclib_version}.jar"
-    println "Downloading ${jarUrl} ..."
-    jarFile.parentFile.mkdirs()
-    new URL(jarUrl).withInputStream { inputStream ->
-        jarFile.withOutputStream { outputStream ->
-            outputStream << inputStream
-        }
-    }
-    println "Download completed: $jarFile"
-}
-```
-
-## Refer to me: `YCLib.xxx`
-- For Architectury projects:
-
-```groovy
-// insert into
-subprojects {
-    repositories {
-        flatDir { dir "../yclib" }
-    }
-    dependencies {
-        modImplementation "yclib:yclib:$yclib_version"
-        if (project.name != 'common') include "yclib:yclib:$yclib_version"
-    }
-}
-```
-
-- For single-platform projects:
-
-```groovy
-// insert into
-repositories {
-    flatDir { dir "yclib" }
-}
-dependencies {
-    // fabric
-    modImplementation "yclib:yclib:$yclib_version"
-    include "yclib:yclib:$yclib_version"
-    // forge
-    implementation "yclib:yclib:$yclib_version"
-    jarJar "yclib:yclib:$yclib_version"
-}
+apply from: 'https://raw.githubusercontent.com/YukkuriC/YCLib-Minecraft/refs/heads/main/download_me.groovy'
 ```
--- download_me.groovy
+++ download_me.groovy
@@ -1,3 +1,5 @@
+if (!project.hasProperty('yclib_version')) throw new GradleException("must add `yclib_version` (e.g. `1.21.1-0.2`) to gradle.properties")
+
 def jarFile = file("$projectDir/yclib/yclib-${yclib_version}.jar")
 if (!jarFile.exists()) {
     def jarUrl = "https://github.com/YukkuriC/YCLib-Minecraft/releases/download/$yclib_version/yclib-${yclib_version}.jar"
@@ -9,4 +11,33 @@ if (!jarFile.exists()) {
         }
     }
     println "Download completed: $jarFile"
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        flatDir { dir "$rootDir/yclib" }
+    }
+
+    afterEvaluate {
+        def depKey = "yclib:yclib:$yclib_version"
+        dependencies {
+            def cfg = project.configurations.findByName('modImplementation')
+                    ?: project.configurations.findByName('implementation')
+            if (cfg) {
+                project.dependencies.add(cfg.name, depKey)
+            } else {
+                println "Skipping dep creation on $name"
+            }
+            if (name != 'common') {
+                def nestCfg = project.configurations.findByName('jarJar')
+                        ?: project.configurations.findByName('include')
+                if (nestCfg) {
+                    project.dependencies.add(nestCfg.name, depKey)
+                } else {
+                    println "Skipping dep nesting on $name"
+                }
+            }
+        }
+    }
 }
